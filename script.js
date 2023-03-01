74340e1a7b7967fdcd3fd0421383bd4cf092aae0
Martin <martinio@hotmail.se>
2023-03-01 08:43:00 +0100

    First

---
+++ script.js
@@ -0,0 +1,34 @@
+"use strict";
+const track = document.getElementById("image-track");
+
+track.dataset.percentage = 0; // set initial value to 0
+
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";
+  track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+const handleOnMove = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+  
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 50), -50) || 0;
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.style.transform = `translate(${nextPercentage}%, -50%) translateX(-50%)`;
+  
+  for(const image of track.getElementsByClassName("image")) {
+    image.style.objectPosition = `${50 + nextPercentage }% center`;
+  }
+}
+
+window.onmousedown = e => handleOnDown(e);
+window.onmouseup = e => handleOnUp(e);
+window.onmousemove = e => handleOnMove(e);
